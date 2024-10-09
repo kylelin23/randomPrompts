33771aad87de7cd7ae9193892fee20524538affb
Kyle Lin <linkyle0924@gmail.com>
2024-10-09 16:52:50 -0700

    Add squish button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,27 @@
 import { Text, StyleSheet, View } from 'react-native';
+import { SquishButton } from 'react-native-squish-button';
+import * as Svg from 'react-native-svg';
 
 export default function HomeScreen() {
   return (
     <View style = {styles.screenContainer}>
-      <Text>Random! </Text>
+      <Text style = {styles.generated}>Jeongin</Text>
+      <View style = {{height: 100}}>
+        <SquishButton
+          height = {100}
+          width = {180}
+          color="#4E5372"
+          squish={7}
+          radius={5}
+          text="Generate!"
+          textStyle={{
+            color: 'white',
+            fontFamily: 'Helvetica',
+            fontWeight: 'bold',
+            fontSize: 16,
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -12,7 +30,15 @@ const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
     backgroundColor: 'pink',
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  generated: {
+    fontSize: 70,
+  },
+
+  container2: {
+    backgroundColor: 'green',
   },
 });
